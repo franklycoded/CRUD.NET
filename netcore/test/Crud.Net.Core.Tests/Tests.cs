cfddfc74d0e9353ff2254f7338971365eb606541
FranklyCoded <peter.frank@franklycoded.com>
2016-07-24 18:39:30 +0100

    Made core project releaseable as beta Removed old test project and added new one with latest nunit support.

---
+++ netcore/test/Crud.Net.Core.Tests/Tests.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Crud.Net.Core.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        public Tests()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void Test(){
+
+        }
+    }
+}
